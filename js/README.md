d1bd92ce85d627a27723dab245fe8c143f761bd3
robbryandev <robbryandev@gmail.com>
2022-10-25 16:05:19 -0700

    Add initial text analyzer
    Co-authored-by: Robert Onstott <roonstott@gmail.com>

---
+++ js/README.md
@@ -0,0 +1,8 @@
+Describe: function numberOfOccurrencesInText(word, text) 
+
+Test: "Return 0 when 'word' parameter is empty"
+Code:
+const word = ""
+const text = "This is a bit of text"
+numberOfOccurrencesInText(word, text) 
+Expected Output: 0
---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+// Business Logic
+
+function wordCounter(text) {
+    if (text.trim().length === 0) {
+        return 0;
+    }
+    let wordCount = 0;
+    const textArray = text.split(" ");
+    textArray.forEach(function (element) {
+        if (!Number(element)) {
+            wordCount++;
+        }
+    });
+    return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+    if (word.trim().length === 0) {
+        return 0;
+    }
+    const textArray = text.split(" ");
+    console.log(textArray);
+    let wordCount = 0;
+    textArray.forEach(function (element) {
+        if (element.toLowerCase().includes(word.toLowerCase())) {
+            wordCount++;
+        }
+    });
+    return wordCount;
+}
+
+// UI Logic
+
+function handleFormSubmission(p) {
+    p.preventDefault();
+    const passage = document.getElementById("text-passage").value;
+    const word = document.getElementById("word").value;
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    document.getElementById("total-count").innerText = wordCount;
+    document.getElementById("selected-count").innerText = occurrencesOfWord;
+}
+
+window.addEventListener("load", function () {
+    document.querySelector("form#word-counter").addEventListener("submit", handleFormSubmission);
+});
